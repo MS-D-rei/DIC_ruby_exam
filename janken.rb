0a7fbdb7cb46d49bb6e26b66ffe9ed18aa7faf4a
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2021-11-22 18:48:30 +0900

    word.to_i return 0. becareful

--- janken.rb
+++ janken.rb
@@ -5,14 +5,14 @@ class Player
   def hand
     puts '数字を入力してください。'
     puts '0:グー, 1:チョキ, 2:パー'
-    input_hand = gets.chomp.to_i
+    input_hand = gets.chomp
     loop do
-      if [0, 1, 2].include?(input_hand)
-        return input_hand
+      if ['0', '1', '2'].include?(input_hand)
+        return input_hand.to_i
       else
         puts '0〜2の数字を入力してください。'
         puts '0:グー, 1:チョキ, 2:パー'
-        input_hand = gets.chomp.to_i
+        input_hand = gets.chomp
       end
     end
   end
